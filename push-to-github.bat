a12c693e0474a690189321f985b8e95719e8a7c3
linwukang <linwukang@yahoo.com>
2023-07-10 14:02:50 +0800

    f

--- push-to-github.bat
+++ push-to-github.bat
@@ -3,4 +3,4 @@ git pull -rebase origin main
 git commit -m "f"
 git branch -M main
 git remote set-url origin git@github.com:linwukang/TechInsightWeb.git
-git push -f origin main
+git push -u origin main
